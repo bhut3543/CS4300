ded63ce7c20e3963446dd159672de96234e741be
Jason John <jaytj95@gmail.com>
2016-11-22 17:17:15 -0500

    Updated SQL file 2

--- 4300proj_11222016.sql
+++ 4300proj_11222016.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `4300proj` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `4300proj`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: 4300proj
@@ -120,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-22 16:52:04
+-- Dump completed on 2016-11-22 17:17:05
